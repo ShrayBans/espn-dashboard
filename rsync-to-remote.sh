26e55a3324a264692af925404fecd365ba4e6ad4
Shray Bansal <bansalshray@gmail.com>
2018-10-27 13:19:53 -0700

    adding finishing touches to rsync to remote

--- rsync-to-remote.sh
+++ rsync-to-remote.sh
@@ -9,15 +9,14 @@ fi
 REMOTE_SERVER=$1
 
 # To check that it will build properly
-docker build -t test-espn-dashboard .
+$(aws ecr get-login --profile sixthman-shraybansal | sed 's/-e none//g') &
+docker build -t 146006631841.dkr.ecr.us-west-1.amazonaws.com/flask-espn-dashboard:latest .
+docker push 146006631841.dkr.ecr.us-west-1.amazonaws.com/flask-espn-dashboard:latest
 
-rsync -azvh ../espn-dashboard $REMOTE_SERVER:espn-dashboard # Only use to instantiate
-rsync -azvh Dockerfile $REMOTE_SERVER:espn-dashboard/Dockerfile
-rsync -azvh src $REMOTE_SERVER:espn-dashboard/src
+# rsync -azvh ../espn-dashboard $REMOTE_SERVER:espn-dashboard # Only use to instantiate
 rsync -azvh docker-compose-espn-dashboard.yml $REMOTE_SERVER:espn-dashboard/docker-compose-espn-dashboard.yml
 
 ssh $REMOTE_SERVER << EOF
     cd espn-dashboard
-    docker build -t flask-espn-dashboard .
     docker-compose -f docker-compose-espn-dashboard.yml up -d
 EOF
